fa2dbcd54d1bd1116e74c755c7eefc5b8611f8cd
Nnamdi Iregbulem <30379073+whoisnnamdi@users.noreply.github.com>
2021-06-20 13:30:42 -0700

    No shadow on post previews above small screens

--- components/postpreview.js
+++ components/postpreview.js
@@ -5,7 +5,7 @@ export default function PostPreview ({ post }) {
     return (
         <div className="">
             <Link href="[slug]" as={`/${post.slug}`}>
-                <a className="flex flex-col sm:flex-row justify-between rounded-lg shadow-md transition duration-500 ease-in-out transform hover:opacity-80 mb-4">
+                <a className="flex flex-col sm:flex-row justify-between rounded-lg shadow-md sm:shadow-none transition duration-500 ease-in-out transform hover:opacity-80 mb-4">
                     <img
                         src={post.feature_image}
                         className="flex-1 sm:w-1/2 rounded-t-lg sm:rounded-l-lg" 
