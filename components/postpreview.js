6c4311a08545d1c713a04327728cafe701680856
Nnamdi Iregbulem <30379073+whoisnnamdi@users.noreply.github.com>
2021-06-25 11:38:53 -0700

    And again

--- components/postpreview.js
+++ components/postpreview.js
@@ -5,7 +5,7 @@ export default function PostPreview ({ post }) {
     return (
         <div className="">
             <Link href="[slug]" as={`/${post.slug}`}>
-                <a className="flex flex-col sm:flex-row sm:max-h-80 justify-between rounded-xl shadow-md sm:shadow-none transition duration-500 ease-in-out transform hover:opacity-80 mb-4">
+                <a className="flex flex-col sm:flex-row sm:max-h-64 justify-between rounded-xl shadow-md sm:shadow-none transition duration-500 ease-in-out transform hover:opacity-80 mb-4">
                     <img
                         src={post.feature_image}
                         className="flex-1 sm:w-1/2 rounded-t-xl sm:rounded-l-xl sm:rounded-r-none" 
