0e3623252cac7fc85c317b9da7a79ca216a89961
Nnamdi Iregbulem <30379073+whoisnnamdi@users.noreply.github.com>
2021-06-26 14:47:02 -0700

    Post preview alt tags

--- components/postpreview.js
+++ components/postpreview.js
@@ -8,6 +8,7 @@ export default function PostPreview ({ post }) {
                 <a className="flex flex-col sm:flex-row sm:max-h-64 justify-between rounded-xl shadow-md sm:shadow-none transition duration-500 ease-in-out transform hover:opacity-80 mb-4">
                     <img
                         src={post.feature_image}
+                        alt={post.title}
                         className="flex-1 sm:w-1/2 rounded-t-xl sm:rounded-l-xl sm:rounded-r-none" 
                     />
                     <div className="flex flex-col items-start place-content-center flex-1 px-3 py-4 sm:px-8 sm:w-1/2 content-center">
