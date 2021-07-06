b6283972aa3c357901c19d4215380d9f3b5a5204
Nnamdi Iregbulem <30379073+whoisnnamdi@users.noreply.github.com>
2021-07-06 12:44:06 -0700

    Link converter add IP address

--- components/linkconverter.js
+++ components/linkconverter.js
@@ -3,15 +3,17 @@ import { useEffect } from 'react'
 
 export default function LinkConverter({ content }) { 
     const router = useRouter()
-    const host = process.env.NEXT_PUBLIC_HOST_URL
+    const host_url = process.env.NEXT_PUBLIC_HOST_URL
+    const host_ip = process.env.NEXT_PUBLIC_HOST_IP
     
     useEffect(() => {
         let links = document.querySelectorAll("a")
         
         try {
             links.forEach((link) => {
-                if (link.href.includes(host)) {
-                    link.href = link.href.replace(host + "/", "/")
+                if (link.href.includes(host_url) || link.href.includes(host_ip)) {
+                    link.href = link.href.replace(host_url + "/", "/")
+                    link.href = link.href.replace(host_ip + "/", "/")
                     link.addEventListener("click", (e) => {
                         e.preventDefault()
                         router.push(e.target.href)
