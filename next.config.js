2549727bb6bcc4644d7fef6e335a5e32cdddedb1
Nnamdi Iregbulem <30379073+whoisnnamdi@users.noreply.github.com>
2021-07-07 19:25:16 -0700

    Add cloudinary to image domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     images: {
-      domains: ['whoisnnamdi.com', '138.68.29.23'],
+      domains: ['whoisnnamdi.com', '138.68.29.23', 'res-2.cloudinary.com', 'res.cloudinary.com'],
     },
     i18n: {
       locales: ["en"],
