816b3792db19749197d890c4b643324e352bd617
Nnamdi Iregbulem <30379073+whoisnnamdi@users.noreply.github.com>
2022-09-24 22:14:39 +0000

    Post preview border, delete shadows

--- components/postpreview.js
+++ components/postpreview.js
@@ -5,7 +5,7 @@ export default function PostPreview ({ post }) {
     return (
         <div className="">
             <Link href="[slug]" as={`/${post.slug}/`}>
-                <a className="flex flex-col sm:flex-row sm:max-h-64 justify-between rounded-xl shadow-md sm:shadow-none transition duration-500 ease-in-out transform hover:opacity-80 mb-4">
+                <a className="flex flex-col sm:flex-row sm:max-h-64 justify-between rounded-xl transition duration-500 ease-in-out transform hover:opacity-80 mb-4 border border-black border-opacity-10">
                     <div className="relative block w-full h-44 sm:w-1/2 sm:h-56 my-auto">
                         <Image
                             src={post.feature_image}
