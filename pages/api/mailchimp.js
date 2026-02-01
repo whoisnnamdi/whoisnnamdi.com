85793f44e54d76944d3e3fc67d19e037c6243a73
whoisnnamdi <nnamdi.iregbulem@gmail.com>
2026-02-01 10:07:27 -0800

    Remove unused code: email variable and getAll alias

--- pages/api/mailchimp.js
+++ pages/api/mailchimp.js
@@ -1,19 +1,17 @@
-import mailchimp from '@mailchimp/mailchimp_marketing'
+import mailchimp from "@mailchimp/mailchimp_marketing";
 
 mailchimp.setConfig({
-    apiKey: process.env.MAILCHIMP_API_KEY,
-    server: process.env.MAILCHIMP_API_SERVER
-})
+  apiKey: process.env.MAILCHIMP_API_KEY,
+  server: process.env.MAILCHIMP_API_SERVER,
+});
 
 export default async function ping(req, res) {
-    const { email } = req.body
+  try {
+    const response = await mailchimp.ping.get();
+    console.log(response);
 
-    try {
-        const response = await mailchimp.ping.get()
-        console.log(response)
-
-        return res.status(201).json({ error: '' })
-    } catch (err) {
-        return res.status(500).json({ error: err.message || err.toString() })
-    }
+    return res.status(201).json({ error: "" });
+  } catch (err) {
+    return res.status(500).json({ error: err.message || err.toString() });
+  }
 }
